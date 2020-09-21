789d6e885547b6b7a4c0f2bf025ae98b568ecbe0
apawitan1995 <arif.pawitan@gmail.com>
2020-09-21 17:33:00 +0900

    tambah variabel

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades" // := only used for new variable
+	card = "Five of Diamonds"
 	fmt.Println(card)
 }
