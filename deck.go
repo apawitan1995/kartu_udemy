77fda3265bfde10ec742fe643e884f9edff73f10
apawitan1995 <arif.pawitan@gmail.com>
2020-09-21 19:08:40 +0900

    Last version Today

--- deck.go
+++ deck.go
@@ -6,9 +6,28 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits { // replace the index with underscores to tell go we dont care
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
